d330d148ad7d56bd10201c5ae47332cb4c829eb6
Martin Lacsamana <77639673+Martinlacsamana@users.noreply.github.com>
2024-08-03 21:19:30 -0400

    Time: 36 ms (82.6%), Space: 12.5 MB (38.08%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        # add the difference (key) and index of the number
+        for index, num in enumerate(nums):
+            # check if target - num is in hashmap
+            diff = target - num
+            if diff in seen:
+                return [index, seen.get(diff)]
+            
+            # else, just add to hashmap
+            seen[num] = index
+        
+        # NOTES
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
+        # Check to see if the complement -> target - num is in the hashmap
+        # If not, just add the number to this dictionary
