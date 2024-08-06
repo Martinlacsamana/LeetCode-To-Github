790110c34ace4bbeb03cc94e87a617df3f17a0be
Martin Lacsamana <77639673+Martinlacsamana@users.noreply.github.com>
2024-08-06 01:01:22 -0400

    Time: 194 ms (36.98%), Space: 18.1 MB (27.74%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,18 +1,24 @@
 class Solution:
+
+    # Strat: Use Binary Search
+    # Time Complexity: O(logn)
+    # Space Complexity: O(1) -> Didn't allocate extra memory
     def search(self, nums: List[int], target: int) -> int:
 
-        # Set boundaries
+        # Set our binary search boundaries
         left, right = 0, len(nums) - 1
 
+        # Continue searching while the left pointer is less than or equal to the right pointer
         while left <= right:
             mid = (left + right) // 2
-            if nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
+            if nums[mid] == target:
                 return mid
-        
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        # If the target is not found, return -1
         return -1
 
 
